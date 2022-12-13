3043906728ad099d2bcadb46130f86f78c19cb67
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-12-13 16:26:25 -0600

    Add files via upload

---
+++ c2-p3-iterating-over-a-list-or-tuple.py
@@ -0,0 +1,9 @@
+l = [1,2,3]
+
+for item in l:
+    print(item)
+
+for index, item22 in enumerate(l,start=2): #enumerate
+    #print(index) # se puede imprimir ambas variables solo con coma
+    print(index, item22)
+
