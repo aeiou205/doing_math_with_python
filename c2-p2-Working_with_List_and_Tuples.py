25c0656e4296febcdfe22248f421d28f6c4a0f65
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-12-13 16:21:56 -0600

    Add files via upload

---
+++ c2-p2-Working_with_List_and_Tuples.py
@@ -0,0 +1,32 @@
+
+#capitulo 2 Visualizing Data with Graphs
+#capitulo 6 Drawing Geometric Shapes and Fractalprint()
+simplelist = [1,2,3]
+print(simplelist[0])
+print(simplelist[1])
+print(simplelist[2])
+
+stringlist = ['a string', 'b string', 'c string']
+print(stringlist)
+print(stringlist[1])
+print(stringlist[2])
+
+emptylist = [] 
+print(emptylist) #no hay nada en la lista
+
+emptylist.append(1)
+print(emptylist)
+
+emptylist.append(5)
+print(emptylist)
+
+
+emptylist.append(3)
+print(emptylist)
+
+simpletuple = (1,2,3)
+print(simpletuple)
+print(simpletuple[2])
+
+
+
