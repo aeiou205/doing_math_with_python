cfdcaacba21982548e3b5bca0a231f82db7c0098
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-12-13 22:40:55 -0600

    Add files via upload

---
+++ c2- p6-Graphing-the-Average-Annual-Temperature-in-New-York-city.py
@@ -0,0 +1,8 @@
+from pylab import plot, show
+
+emp = [53.9, 56.3, 56.4, 53.4, 54.5, 55.8, 56.8, 55.0, 55.3, 54.0, 56.7, 56.4, 57.3]
+nyc_temp_200 = [40.p]
+years = range(2000, 2013)
+plot(years, emp, marker='o')
+
+show()
